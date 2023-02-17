533bdcdd0909dea5767304ceb4d4b3982b6a4423
AlbyRams <AlbyRams@github.com>
2023-02-17 09:03:40 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,13 @@ char charType;
 int intType;
 long int longintType;
 long long int longlongintType;
-float floattype;
+float floatType;
 
-printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
-printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
-Printf("size of a long int: %zu byte(s)\n", sizeof(longinttype));
-printf("size of a long long int: %zu byte(s)\n", sizeof(longlonginttype));
-printf("size of a float: %zu byte(s)\n", sizeof(floattype));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+Printf("size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 }
