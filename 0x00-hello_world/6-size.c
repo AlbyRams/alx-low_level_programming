cf64bb8a765c628f83bd2e4006071b9b2c3db555
AlbyRams <AlbyRams@github.com>
2023-02-17 09:18:14 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - A program that prints the size of various computer types
+ * main - A program that prints various computer sizes
  *
  * Return: Always 0 (Success)
  */
@@ -13,11 +13,11 @@ long int longintType;
 long long int longlongintType;
 float floatType;
 
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-Printf("size of a long int: %zu byte(s)\n", sizeof(longintType));
-printf("size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
-printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+Printf("size of long int: %zu byte(s)\n", sizeof(longintType));
+printf("size of long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("size of float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 }
